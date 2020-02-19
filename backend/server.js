b6bcaf1ef0729fa248243f6b7d616b1e092406ad
Ankit <ankmisra@deloitte.com>
2020-02-19 13:59:37 +0530

    Match logic test

--- backend/server.js
+++ backend/server.js
@@ -59,10 +59,10 @@ app.post("/activity/execute", (req, res) => {
           .then(function(obj) {
 			console.log(obj);
 			var i;
-            for (i = 0; i < parseInt(Object.keys(obj.content)); i++) {
+            for (i = 0; i < (Object.keys(obj.content).length); i++) {
 				console.log(i);
               if (obj.content[i].CUSTOMER_INDID == "12345") {
-				  console.log(obj.content[i].CUSTOMER_INDID);
+				 console.log(obj.content[i].CUSTOMER_INDID);
                 switch (String(obj.content[i].segmentValue)) {
                   case "verylikely":
                     return res.status(200).json({
